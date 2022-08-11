3902dd31ced6aa6724aab6691b473478cc461d76
Ubuntu <ubuntu@ip-172-31-31-157.us-east-2.compute.internal>
2022-08-11 00:20:46 +0000

    with user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
-    
+    # validates if username is present before saving a user object
+    # uniqueness is by default case sensitive
+    validates :username, presence: true, uniquness: {case_sensitive: false}, length: {minimum: 3, maximum: 25}
+    #store regular expresssion here. test regex expression at rubular.com 
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, uniquness: {case_sensitive: false}, 
+        length: {maximum: 50},
+        format: {with: VALID_EMAIL_REGEX}
 end
